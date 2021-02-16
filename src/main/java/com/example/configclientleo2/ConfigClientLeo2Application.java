a96a162ebf250aaba5351d84186de0da46869b49
Leonardo de Souza <leosz.souza@gmail.com>
2021-02-16 11:25:59 -0300

    initial structure

---
+++ src/main/java/com/example/configclientleo2/ConfigClientLeo2Application.java
@@ -0,0 +1,13 @@
+package com.example.configclientleo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientLeo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientLeo2Application.class, args);
+	}
+
+}
